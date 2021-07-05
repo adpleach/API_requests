faf98e076088fdd670e25df29964fb08b0a48c9b
adpleach <adpleach@gmail.com>
2021-07-05 13:27:21 -0400

    All data collected

--- request.py
+++ request.py
@@ -5,7 +5,7 @@ Created on Sun Jul  4 09:05:20 2021
 @author: adple
 """
 
-#https://www.youtube.com/watch?v=1lxrb_ezP-g&ab_channel=CoreySchafer
+# https://www.youtube.com/watch?v=1lxrb_ezP-g&ab_channel=CoreySchafer
 
 import pandas as pd
 import requests
@@ -13,7 +13,7 @@ from requests.auth import HTTPBasicAuth
 
 r = requests.get('url')
 courses_json = r.json()
-courses = pd.DataFrame(courses_json) 
+courses = pd.DataFrame(courses_json)
 # course_users = json.dumps(course_json) - alternative using json library
 
 print(courses.columns)
@@ -22,16 +22,22 @@ df_course = pd.DataFrame()
 
 for i in courses['id']:
     course_id = i
-    course_url = f'url/{course_id}'
-    r = requests.get(course_url, auth=HTTPBasicAuth('user', 'password'))
+    course_url = f'url/{course_id}/users'
+    r = requests.get(course_url, auth=HTTPBasicAuth(
+        'user', 'password'))
     course_json = r.json()
-    print(course_json)
-    # course = pd.DataFrame(course_json)
-    # df_course.append(course)
+    course = pd.DataFrame(course_json)
+    df_course = df_course.append(course)
 
-# print(df_course.head())
+df_user = pd.DataFrame()
 
-# r = requests.get(course_url)
-# course_json = r.json()
+for i in df_course['id']:
+    user_id = i
+    user_url = f'url/{user_id}/course-progress'
+    r = requests.get(user_url, auth=HTTPBasicAuth(
+        'user', 'password'))
+    user_json = r.json()
+    user = pd.DataFrame(user_json)
+    df_user = df_user.append(user)
 
-# print(course_users)
+print(df_user.head())
